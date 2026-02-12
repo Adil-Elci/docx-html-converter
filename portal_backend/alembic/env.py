1f5d0c816fe293c6f1156a7f3b935ba3e5f24127
Adil-Elci.AI <aat@elci.cloud>
2026-02-12 18:14:58 +0400

    Use portal_backend package import in Alembic env

--- portal_backend/alembic/env.py
+++ portal_backend/alembic/env.py
@@ -6,7 +6,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from api.portal_models import Base
+from portal_backend.api.portal_models import Base
 
 config = context.config
 
